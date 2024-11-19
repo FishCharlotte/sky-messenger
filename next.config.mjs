d19f530bcb01133250a8a05eb13c941910d5da2e
FishCharlotte <poonsiuyu@126.com>
2024-11-19 16:37:20 +0800

    Feature: Added some notes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 import BuilderDevTools from "@builder.io/dev-tools/next";
 
 /** @type {import('next').NextConfig} */
+// 如果不想build，请将这个配置留空！不然无法npm start！
 const nextConfig = BuilderDevTools()({
-    basePath: "/ec5001-sky-messenger",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    // basePath: "/ec5001-sky-messenger",
+    // output: "export",  // <=== enables static exports
+    // reactStrictMode: true,
 });
 
 export default nextConfig;
